f4c4263a6993ab9cf138fee31c1b4633998fc303
redoundo <vivith0987@gmail.com>
2025-07-23 17:40:24 +0900

    add fakeVLM root path

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -1,3 +1,9 @@
+import sys
+file_path: str = __file__.replace("\\", "/")
+root_path: str = file_path.split("FakeVLM")[0]
+sys.path.append(root_path + "FakeVLM")
+sys.path.append(root_path + "FakeVLM/llava")
 from llava.train.train import train
+
 if __name__ == "__main__":
     train(attn_implementation="flash_attention_2")
