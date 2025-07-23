8511d713f059345c9f9587370f4aeeb5aed67440
redoundo <vivith0987@gmail.com>
2025-07-23 18:01:45 +0900

    add all path

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -3,6 +3,10 @@ file_path: str = __file__.replace("\\", "/")
 root_path: str = file_path.split("FakeVLM")[0]
 sys.path.append(root_path + "FakeVLM")
 sys.path.append(root_path + "FakeVLM/llava")
+sys.path.append(root_path + "FakeVLM/llava/model")
+sys.path.append(root_path + "FakeVLM/llava/model/language_model")
+sys.path.append(root_path + "FakeVLM/llava/model/multimodal_encoder")
+sys.path.append(root_path + "FakeVLM/llava/model/multimodal_projector")
 from llava.train.train import train
 
 if __name__ == "__main__":
