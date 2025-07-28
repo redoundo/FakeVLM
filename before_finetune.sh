bc044618c60540464119bb600625b2e4a937aeee
redoundo <vivith0987@gmail.com>
2025-07-28 13:28:57 +0900

    ..

--- before_finetune.sh
+++ before_finetune.sh
@@ -2,6 +2,7 @@
 
 mkdir /root/FakeVLM/checkpoints
 pip install -r /root/FakeVLM/requirements.txt
+python -m pip install -q "https://download.pytorch.org/whl/cu121/torch-2.1.2%2Bcu121-cp310-cp310-linux_x86_64.whl#sha256=b2184b7729ef3b9b10065c074a37c1e603fd99f91e38376e25cb7ed6e1d54696"
 pip install -q flash-attn==2.7.3 --no-build-isolation
 pip install -U "huggingface_hub[cli]"
 hf auth login
--- create_env.sh
+++ create_env.sh
@@ -5,4 +5,6 @@ conda create -n fakevlm python=3.10 -y
 conda init
 source /root/.bashrc
 source /root/.bashrc
+export PATH=/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+cd /root/FakeVLM
 conda activate fakevlm
